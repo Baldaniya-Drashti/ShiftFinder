44e8443dbb1e0040744cabf296e9010ca811be78
Dhaval <dhaval@iroidsolutions.com>
2024-10-25 03:00:34 -0700

    - Changes

--- lib/presentation/billing/transaction_info.dart
+++ lib/presentation/billing/transaction_info.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:shift/presentation/common/widgets/base_text.dart';
 
 class TransactionInfo extends StatelessWidget {
-  const TransactionInfo({super.key, required this.label, required this.value});
+  const TransactionInfo({
+    super.key,
+    required this.label,
+    required this.value,
+    this.valueColor,
+  });
 
   final String label;
   final String value;
+  final Color? valueColor;
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +24,43 @@ class TransactionInfo extends StatelessWidget {
             child: BaseText(text: "$label :", fontSize: 10, fontWeight: FontWeight.w500),
           ),
           Expanded(
-            child: BaseText(text: value, fontSize: 10, fontWeight: FontWeight.w600),
+            child: BaseText(
+              text: value,
+              fontSize: 10,
+              fontWeight: FontWeight.w600,
+              textColor: valueColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MonthlyStatementInfo extends StatelessWidget {
+  final String label;
+  final String value;
+  final Color? valueColor;
+  const MonthlyStatementInfo({super.key, required this.label, required this.value, this.valueColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(flex: 2,
+            child: BaseText(text: "$label :", fontSize: 12, fontWeight: FontWeight.w500),
+          ),
+          Expanded(
+            child: BaseText(
+              text: value,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              textColor: valueColor,
+              textAlign: TextAlign.end,
+            ),
           ),
         ],
       ),
